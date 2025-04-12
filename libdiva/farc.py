1098cf9426288b760c243724a7c1fe805fcb0c26
memoryleak <glupacsuper2009@gmail.com>
2025-04-12 10:43:23 +0200

    implemented file list parser.... sort of

--- libdiva/farc.py
+++ libdiva/farc.py
@@ -1,31 +1,41 @@
 import io
 import struct
-
-print("Debug: program worky!!")
+import sys
 
 class FARCExtract:
   def __init__(self, filepath):
     self.filepath = filepath
     self.f = None
-
-  def open(self):
-    self.f = open(self.filepath, "rb")
-
-  def close(self):
-    if self.f:
-      self.f.close()
-      self.f = None
+    self.filenames = []
 
   def check_header(self):
-    try:
-      self.open()
+    with open(self.filepath, 'rb') as f:
+      self.f = f
+      self.f.seek(0)
       header = self.f.read(4)
-      print(f"Header: {header}")
-      if header != b'FARC':
-        raise ValueError("invalid FARC header. check for corruption or wrong file type")
-      return True
-    finally:
-      self.close()
+      if header !=b'FARC':
+        raise ValueError("invalid header. is this a valid farc file?")
+
+  def parse_flist(self):
+    with open(self.filepath, 'rb') as f:
+      f.seek(0x28)
+
+      while True:
+        chunk = f.read(4)
+        if chunk == b"xxxx":
+          break
+
+        f.seek(-4, io.SEEK_CUR)
+
+        entry = f.read(32) # all filenames seem to be allocated a 32 byte space within the file list
+        filename = entry.split(b'\x00')[0].decode('ascii', errors='ignore')
+        self.filenames.append(filename)
+
+        f.read(8)
+
+  def print_flist(self):
+    for name in self.filenames:
+      print(name)
 
 if __name__ == "__main__":
   import sys
@@ -36,9 +46,11 @@ if __name__ == "__main__":
   farc = FARCExtract(sys.argv[1])
   try:
     farc.check_header()
-    print("checked header")
+    print("info: valid header found.")
+    farc.parse_flist()
+    print(f"info: parsed files: ")
+    farc.print_flist()
   except ValueError as e:
     print(f"Error: {e}")
     sys.exit(1)
-    
     
